b93be072e865326ec09aa4d102deb85ce3855813
Anes Batijari <anesbatijari18@gmail.com>
2026-02-19 16:00:22 +0100

    Update Potenzial.php

--- app/Models/Potenzial.php
+++ app/Models/Potenzial.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Potenzial extends Model
 {
-    //
+    protected $fillable = [
+        'client_name',
+        'url',
+        'language_id',
+        'location_id',
+        'keywords',
+        'job_id',
+        'status',
+        'error_message',
+        'file_potenzial',
+    ];
+
+    public function language()
+    {
+        return $this->belongsTo(Language::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(Location::class);
+    }
 }
